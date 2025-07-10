694ff6d6bcd5cbcec9ce24dc09aa457e36d67284
max-hubenko <maksymhub@gmail.com>
2025-07-10 13:08:25 -0500

    feat: Added business license scheduler

--- infra/schedulers.tf
+++ infra/schedulers.tf
@@ -51,3 +51,22 @@ resource "google_cloud_scheduler_job" "zoning_job" {
         }
     }
 }
+
+
+################# BUSINESS LICENSES DATA SCHEDULER
+
+resource "google_cloud_scheduler_job" "business_licenses_job" {
+    name             = "ingest-chicago-business-licenses-job"
+    description      = "Triggers the ingest-chicago-business-licenses function every 3 months"
+    schedule         = "0 3 1 1,4,7,10 *" # minute 0, 3AM, on the first day, of Jan, Apr, Jul, Oct (every 3 months)
+    time_zone        = "America/Chicago"
+
+    http_target {
+        http_method = "POST"
+        uri         = google_cloudfunctions_function.business_licenses_function.https_trigger_url
+
+        oidc_token {
+        service_account_email = google_service_account.scheduler_invoker.email
+        }
+    }
+}
