3a39cd1c0694d986b0eacf967fe74824c79348fa
max-hubenko <maksymhub@gmail.com>
2025-07-10 11:52:50 -0500

    feat: implemented zoning data scheduler

--- infra/schedulers.tf
+++ infra/schedulers.tf
@@ -16,6 +16,7 @@ resource "google_cloud_scheduler_job" "demographics_job" {
     }
 }
 
+################# FOOT TRAFFIC DATA SCHEDULER
 
 resource "google_cloud_scheduler_job" "foot_traffic_job" {
     name             = "ingest-chicago-foot-traffic"
@@ -26,6 +27,24 @@ resource "google_cloud_scheduler_job" "foot_traffic_job" {
     http_target {
         http_method = "POST"
         uri         = google_cloudfunctions_function.foot_traffic_function.https_trigger_url
+
+        oidc_token {
+        service_account_email = google_service_account.scheduler_invoker.email
+        }
+    }
+}
+
+################# ZONING DATA SCHEDULER
+
+resource "google_cloud_scheduler_job" "zoning_job" {
+    name             = "ingest-chicago-zoning-job"
+    description      = "Triggers the ingest-chicago-zoning function every 3 months"
+    schedule         = "0 3 1 1,4,7,10 *" # minute 0, 3AM, on the first day, of Jan, Apr, Jul, Oct (every 3 months)
+    time_zone        = "America/Chicago"
+
+    http_target {
+        http_method = "POST"
+        uri         = google_cloudfunctions_function.zoning_function.https_trigger_url
 
         oidc_token {
         service_account_email = google_service_account.scheduler_invoker.email
