4b52c750eb379adf409ff563007b0e58131c4c18
max-hubenko <maksymhub@gmail.com>
2025-07-10 13:28:58 -0500

    feat: implemented CTA stations, Divvy Stations, and Food inspection scheduler

--- infra/schedulers.tf
+++ infra/schedulers.tf
@@ -70,3 +70,61 @@ resource "google_cloud_scheduler_job" "business_licenses_job" {
         }
     }
 }
+
+
+################# FOOD INSPECTIONS DATA SCHEDULER
+
+resource "google_cloud_scheduler_job" "food_inspections_job" {
+    name             = "ingest-chicago-food-inspections-job"
+    description      = "Triggers the ingest-chicago-food-inspections function every 3 months"
+    schedule         = "0 3 1 1,4,7,10 *" # minute 0, 3AM, on the first day, of Jan, Apr, Jul, Oct (every 3 months)
+    time_zone        = "America/Chicago"
+    attempt_deadline = "1200s" # 20 minutes
+
+    http_target {
+        http_method = "POST"
+        uri         = google_cloudfunctions_function.food_inspections_function.https_trigger_url
+
+        oidc_token {
+        service_account_email = google_service_account.scheduler_invoker.email
+        }
+    }
+}
+
+
+################# DIVVY STATION DATA SCHEDULER
+
+resource "google_cloud_scheduler_job" "divvy_stations_job" {
+    name             = "ingest-chicago-divvy-stations-job"
+    description      = "Triggers the ingest-chicago-divvy-stations function every 3 months"
+    schedule         = "0 3 1 1,4,7,10 *" # minute 0, 3AM, on the first day, of Jan, Apr, Jul, Oct (every 3 months)
+    time_zone        = "America/Chicago"
+
+    http_target {
+        http_method = "POST"
+        uri         = google_cloudfunctions_function.divvy_stations_function.https_trigger_url
+
+        oidc_token {
+        service_account_email = google_service_account.scheduler_invoker.email
+        }
+    }
+}       
+
+################# CTA BUS STATION DATA SCHEDULER
+
+resource "google_cloud_scheduler_job" "cta_bus_stations_job" {
+    name             = "ingest-chicago-cta-bus-stations-job"
+    description      = "Triggers the ingest-chicago-cta-bus-stations function every 3 months"
+    schedule         = "0 3 1 1,4,7,10 *" # minute 0, 3AM, on the first day, of Jan, Apr, Jul, Oct (every 3 months)
+    time_zone        = "America/Chicago"
+    
+
+    http_target {
+        http_method = "POST"
+        uri         = google_cloudfunctions_function.cta_bus_stations_function.https_trigger_url
+
+        oidc_token {
+        service_account_email = google_service_account.scheduler_invoker.email
+        }
+    }
+}
